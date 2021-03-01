eaf2d3673487571998adad1fb1aa82688c131553
wakakusa <wakakusaeng@outlook.jp>
2021-03-01 20:16:25 +0900

    first commit

--- src/githubactions4julia.jl
+++ src/githubactions4julia.jl
@@ -1,5 +1,13 @@
 module githubactions4julia
 
 # Write your package code here.
+"""
+githubactions4juliasample()
+return true
+"""
+function githubactions4juliasample()
+    return true
+end
 
+export githubactions4juliasample
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using githubactions4julia
 using Test
 
-@testset "githubactions4julia.jl" begin
+@testset "githubactions4julia.jl 基本テスト" begin
     # Write your tests here.
+    @test githubactions4juliasample()
 end
